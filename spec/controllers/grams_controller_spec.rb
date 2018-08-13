7bd89f2011c536d5f84dee31d9d22cc72bab9532
0rr <sethwbrown22@gmail.com>
2018-08-13 00:46:20 +0000

    tdd_9

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -23,8 +23,14 @@ RSpec.describe GramsController, type: :controller do
 
       gram = Gram.last
       expect(gram.message).to eq("HelloWorld")
+    end
 
+    it "should properly deal with validation errors" do
+      post :create, params: {gram: {message:''}}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(Gram.count).to eq 0
     end
   end
+
 
 end
