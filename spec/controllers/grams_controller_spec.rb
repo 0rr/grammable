9e000ad88ebb7879f924ac97e6e4d13bb1a0fd79
0rr <sethwbrown22@gmail.com>
2018-08-17 03:54:48 +0000

    tdd_15

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -67,21 +67,60 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#edit action" do
+    it "should not let a user who did not create the gram edit the gram" do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "should not let unauthenticated users edit a gram" do
+      gram = FactoryBot.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to redirect_to new_user_session_path
+    end
+
     it "should successfully show the edit form if the gram is found" do
       gram = FactoryBot.create(:gram)
+      sign_in gram.user
+
       get :edit, params: {id: gram.id}
       expect(response).to have_http_status(:success)
     end
 
     it "should return a 404 error message if the gram is not found" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
       get :edit, params: {id: 'TACOCAT'}
       expect(response).to have_http_status(:not_found)
     end
   end
 
   describe "grams#update action" do
+
+    it "should not let a user who did not create the gram update the gram" do 
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      patch :update, params: { id: gram.id, gram: {message: 'wahoowa'} }
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    #!A - make list of different http statuses, and how to return them in rails/what they are called in rspec/ what they mean in http
+    #!Q - is there a CLI for rspec?
+
+    it "should not let unauthenticated users update a gram" do
+      gram = FactoryBot.create(:gram)
+      patch :update, params: { id: gram.id, gram:{ message: "Hello" } }
+      expect(response).to redirect_to new_user_session_path
+    end
+
     it "should allow users to successfully update grams" do
       gram = FactoryBot.create(:gram, message: "Initial Value")
+      sign_in gram.user
+
       patch :update, params: {id: gram.id, gram: { message: "Changed Value"}}
       expect(response).to redirect_to root_path
       gram.reload
@@ -89,12 +128,17 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should have http 404 error if gram is not found" do 
+      user = FactoryBot.create(:user)
+      sign_in user
+
       patch :update, params: {id: 'foo', gram: {message: "phooey"} }
       expect(response).to have_http_status(:not_found)
     end
 
     it "should render the edit form with an http status of unprocessable_entity" do
       gram = FactoryBot.create(:gram, message: "Initial Value")
+      sign_in gram.user
+
       patch :update, params: {id: gram.id, gram: { message: ""}}
       expect(response).to have_http_status(:unprocessable_entity)
       gram.reload
@@ -103,8 +147,23 @@ RSpec.describe GramsController, type: :controller do
   end
 
 describe "grams#destroy action" do
+  it "should not allow a user that did not create a gram to destory a gram" do
+    gram = FactoryBot.create(:gram)
+    user = FactoryBot.create(:user)
+    sign_in user
+    delete :destroy, params: { id: gram.id }
+    expect(response).to have_http_status(:forbidden)
+  end
+
+  it "should not let unauthenticated users destroy a gram" do
+    gram = FactoryBot.create(:gram)
+    delete :destroy, params: { id: gram.id }
+    expect(response).to redirect_to new_user_session_path
+  end
+
   it "should allow a user to destroy grams" do
     gram = FactoryBot.create(:gram)
+    sign_in gram.user
     delete :destroy, params: {id: gram.id}
     expect(response).to redirect_to root_path
     #!Q - how does this work? We are redining gram as the using the gram.id of the gram that has been destroyed. Do we even need that line, or would the test work with only the second one?
@@ -112,7 +171,9 @@ describe "grams#destroy action" do
     expect(gram).to eq nil
   end
 
-  it "should return a 404 message if we cannot find a gram with the id that is specified" do
+  it "should return a 404 message if we cannot findz a gram with the id that is specified" do
+    user = FactoryBot.create(:user)
+    sign_in user
     delete :destroy, params: {id: "blah blah blah"}
     expect(response).to have_http_status(:not_found)
   end
